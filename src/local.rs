7352e2c5962fa64c97e48999a284852ee81b5979
Connor Monks <monkslc@gmail.com>
2020-09-19 20:40:05 -0400

    change file naming for ephemeral settings update file
    Previously the update file was being temporarily stored in a
    .updated.settings.toml dir. This was unneccessary and now a temp file
    is created in the nodeup config dir.

--- src/local.rs
+++ src/local.rs
@@ -1,15 +1,10 @@
 use crate::target::Target;
-use std::{
-    env, fs, io,
-    path::{Path, PathBuf},
-};
+use std::{env, fs, io, path::PathBuf};
 use tempfile::NamedTempFile;
 use thiserror::Error;
-use uuid::Uuid;
 
 const CONFIG_FILE_NAME: &str = "settings.toml";
 const NODEUP: &str = "nodeup";
-const TRANSITORY_UPDATE_FILE: &str = ".updated.settings.toml";
 
 const CONFIG_DIR_NOT_FOUND: &str = "Can't find an appropriate directory for config. Searched $NODEUP_CONFIG_DIR/settings.toml -> $XDG_CONFIG_HOME/nodeup/settings.toml -> $HOME/.config/nodeup/settings.toml";
 const DOWNLOAD_DIR_NOT_FOUND: &str = "Can't find an appropriate directory for node binaries. Searched $NODEUP_DOWNLOADS -> $XDG_DATA_HOME/nodeup -> $HOME/.local/share/nodeup";
@@ -26,12 +21,10 @@ pub enum LocalError {
     IO { source: io::Error, path: PathBuf },
 }
 
-/*
- * Order of preference for download directory
- * 1. $NODEUP_DOWNLOADS
- * 2. $XDG_DATA_HOME/nodeup
- * 3. $HOME/.local/share/nodeup
- */
+/// Order of preference for download directory
+/// 1. $NODEUP_DOWNLOADS
+/// 2. $XDG_DATA_HOME/nodeup
+/// 3. $HOME/.local/share/nodeup
 pub fn download_dir() -> LocalResult<PathBuf> {
     let nodeup_bin = env::var_os("NODEUP_DOWNLOADS").map(|dir| PathBuf::from(&dir));
     if let Some(nodeup_bin) = nodeup_bin {
@@ -47,48 +40,46 @@ pub fn target_path(target: &Target) -> LocalResult<PathBuf> {
     download_dir().map(|dir| dir.join(target.to_string()))
 }
 
-/*
- * Order of preference for binary directory
- * 1. $NODEUP_CONFIG/settings.toml
- * 2. $XDG_CONFIG_HOME/nodeup/settings.toml
- * 3. $HOME/.config/nodeup/settings.toml
- */
-pub fn config_file() -> LocalResult<PathBuf> {
-    env::var_os("NODEUP_CONFIG")
-        .map(|dir| PathBuf::from(dir).join(CONFIG_FILE_NAME))
-        .or_else(|| dirs::config_dir().map(|dir| dir.join(NODEUP).join(CONFIG_FILE_NAME)))
-        .ok_or(LocalError::NotFound(CONFIG_DIR_NOT_FOUND))
+/// Order of preference for binary directory
+/// 1. $NODEUP_CONFIG
+/// 2. $XDG_CONFIG_HOME/nodeup
+/// 3. $HOME/.config/nodeup
+pub fn config_dir() -> LocalResult<PathBuf> {
+    let config_dir = env::var_os("NODEUP_CONFIG")
+        .map(|dir| PathBuf::from(dir))
+        .or_else(|| dirs::config_dir().map(|dir| dir.join(NODEUP)));
+
+    match config_dir {
+        Some(config_dir) => {
+            // Create config dir in case it doesn't already exist
+            fs::create_dir_all(&config_dir).map_err(|source| LocalError::IO {
+                source,
+                path: config_dir.to_path_buf(),
+            })?;
+            Ok(config_dir)
+        }
+        None => Err(LocalError::NotFound(CONFIG_DIR_NOT_FOUND)),
+    }
 }
 
-/*
- * Transitory config file. Used for writing updates before overwriting the original file. The file
- * will have a randomly generated file name
- */
+pub fn config_file() -> LocalResult<PathBuf> {
+    config_dir().map(|dir| dir.join(CONFIG_FILE_NAME))
+}
+
+/// Transitory config file. Used for writing updates before overwriting the original file. The file
+/// will have a randomly generated file name
 pub fn transitory_config_file() -> LocalResult<NamedTempFile> {
-    let transitory_file_name = Path::new(TRANSITORY_UPDATE_FILE).join(Uuid::new_v4().to_string());
-    let transitory_file_path = env::var_os("NODEUP_CONFIG")
-        .map(|dir| PathBuf::from(dir).join(&transitory_file_name))
-        .or_else(|| dirs::config_dir().map(|dir| dir.join(NODEUP).join(&transitory_file_name)))
-        .ok_or(LocalError::NotFound(CONFIG_DIR_NOT_FOUND))?;
-
-    // Create config dir in case it doesn't already exist
-    fs::create_dir_all(&transitory_file_path).map_err(|source| LocalError::IO {
+    let config_dir = config_dir()?;
+    NamedTempFile::new_in(&config_dir).map_err(|source| LocalError::IO {
         source,
-        path: transitory_file_path.to_path_buf(),
-    })?;
-
-    NamedTempFile::new_in(&transitory_file_path).map_err(|source| LocalError::IO {
-        source,
-        path: transitory_file_path,
+        path: config_dir,
     })
 }
 
-/*
- * Order of preference for download directory
- * 1. $NODEUP_LINKS
- * 2. $XDG_BIN_HOME/nodeup/links
- * 3. $HOME/.local/bin
- */
+/// Order of preference for download directory
+/// 1. $NODEUP_LINKS
+/// 2. $XDG_BIN_HOME/nodeup/links
+/// 3. $HOME/.local/bin
 pub fn links() -> LocalResult<PathBuf> {
     env::var_os("NODEUP_LINKS")
         .map(PathBuf::from)
